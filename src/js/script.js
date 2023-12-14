4922ce86d76ed3d5768d72dc9a017946d6087af6
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2023-12-15 00:10:03 +0200

    HW 2 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+//Task #1
+
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+alert(admin);
+
+//Task #2
+
+let myPlanetName = 'Земля';
+let visitorName = 'Кристина';
+
+//Task #3
+
+let sum = '10' + 10;
+console.log(typeof sum);
+console.log(sum);
