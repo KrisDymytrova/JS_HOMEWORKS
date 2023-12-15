861dac1f363eee1f6ee13bedf01ffdb1e56c150c
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2023-12-15 17:07:01 +0200

    HW 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let a = prompt('Введите первое число');
+let b = prompt('Введите второе число');
+
+
+const sum = parseInt(a) + parseInt(b);
+console.log('Результат: ' + a + '+' + b + ' = ' + sum);
+
+const subtraction = parseInt(a) - parseInt(b);
+console.log('Результат: ' + a + '-' + b + ' = ' + subtraction);
+
+const multiplication = parseInt(a) * parseInt(b);
+console.log('Результат: ' + a + '*' + b + ' = ' + multiplication);
+
+const division = parseInt(a) / parseInt(b);
+console.log('Результат: ' + a + '/' + b + ' = ' + division);
+
