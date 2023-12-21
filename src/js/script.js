186acedcc705189ff60c7769ee1b01da30c457e9
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2023-12-22 01:29:56 +0200

    HW 4 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+//Task 1
+
+const userFirstName = prompt('Введите своё имя');
+const userLastName = prompt('Введите свою фамилию');
+const userPatronymic = prompt('Введите своё отчество');
+
+const userFullName = userLastName + ' ' + userFirstName + ' ' + userPatronymic;
+
+console.log(userFullName);
+
+//Task 2
+
+const myNumber = 12345;
+const myNumberStr = myNumber.toString();
+
+const myNumberDigits = `${myNumberStr[0]} ${myNumberStr[1]} ${myNumberStr[2]} ${myNumberStr[3]} ${myNumberStr[4]}`;
+
+console.log(myNumberDigits);
