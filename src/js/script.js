d5b5ac50bfb6e98b78c5d3c3eded319576d9cf7c
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-02 20:14:21 +0200

    HW 13 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const operation = prompt('Введите вид математической операции, которую хотите выполнить (add, sub, mult, div)');
+let firstNumber = prompt('Введите первое число');
+let secondNumber = prompt('Введите второе число');
+
+firstNumber = parseFloat(firstNumber);
+secondNumber = parseFloat(secondNumber);
+
+if (operation === 'add') {
+    alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+} else if (operation === 'sub') {
+    alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+} else if (operation === 'mult') {
+    alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+} else if (operation === 'div') {
+    if (secondNumber !== 0) {
+        alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+    } else {
+        alert('Делить на ноль нельзя');
+    }
+} else {
+    alert('Данная операция не поддерживается');
+}
