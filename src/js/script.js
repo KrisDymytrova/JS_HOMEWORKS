7014c5119b0e25243a751e506320f23094aca0ed
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-02 20:37:56 +0200

    HW 15 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let firstNumber = prompt('Введите первое число');
+let secondNumber = prompt('Введите второе число');
+let thirdNumber = prompt('Введите второе число');
+
+firstNumber = parseFloat(firstNumber);
+secondNumber = parseFloat(secondNumber);
+thirdNumber = parseFloat(thirdNumber);
+
+const result = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(`Среднее арифметиическое этих чисел: ${result}`);
