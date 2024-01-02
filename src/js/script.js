48174473cab2feb133bae91ecf3008bd94490ebe
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-02 19:17:17 +0200

    HW 12 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+let firstNumber = prompt('Введите первое число');
+let secondNumber = prompt('Введите второе число');
+
+firstNumber = parseFloat(firstNumber);
+secondNumber = parseFloat(secondNumber);
+
+alert(
+    `${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}\n` +
+    `${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}\n` +
+    `${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}\n` +
+    `${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`
+);
