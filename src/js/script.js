d31398678c0f18af9e1ba5f4aac779159c477017
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-08 02:06:10 +0200

    HW 23 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,34 @@
 'use strict';
 
+const arr = [1, 2, 3, 4, 5];
+
+//Сумма элементов массива
+
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log(sum);
+
+
+//Сумма квадратов элементов с помощью оператора **
+
+let sumSquares = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sumSquares += arr[i] ** 2;
+}
+
+console.log(sumSquares);
+
+
+//Сумма квадратов элементов с помощью Math.pow()
+
+let sumSquares2 = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sumSquares2 += Math.pow(arr[i], 2);
+}
+
+console.log(sumSquares2);
