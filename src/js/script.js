fc04a1dfa133691336fc49cf4a60d86764232e82
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-09 03:27:11 +0200

    Format fix

--- src/js/script.js
+++ src/js/script.js
@@ -128,13 +128,10 @@ console.log(sumDivisor);
 //Task 12
 
 for (let i = 1; i <= 10; i++) {
-        for (let b = 1; b <= 10; b++) {
-            console.log(`${i} * ${b} = ${i * b}`);
-        }
+    for (let b = 1; b <= 10; b++) {
+        console.log(`${i} * ${b} = ${i * b}`);
+    }
 }
-
-
-
 
 
 
