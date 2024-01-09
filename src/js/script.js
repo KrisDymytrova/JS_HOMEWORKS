19755af71c10a2c808d1d9b75ceca7e8ecee3e77
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-09 04:06:50 +0200

    HW 24 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const number = +prompt('Введіть число');
+
+if (number <= 1) {
+    console.log('NaN')
+} else {
+    for (let i = 2; i <= number; i++) {
+        if (number % i === 0) {
+            console.log(`Найменший дільник числа ${number} - ${i}`);
+            break;
+        }
+    }
+}
