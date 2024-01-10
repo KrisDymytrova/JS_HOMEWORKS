4ae69bb30091d818b60145b4b523905f8127e936
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-10 02:07:35 +0200

    HW 25 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let sumOfNum = 0;
+
+outerLoop: for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        continue outerLoop;
+    }
+    sumOfNum += i;
+}
+
+console.log(sumOfNum);
+
