99e0f64331fb2604ff63362ae10223c2a0dadf3e
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-11 03:16:22 +0200

    HW 27 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const number = +prompt('Введіть число:');
+const exponent = +prompt('Введіть показник ступеня, до якого слід піднести число:');
+
+function numberToExponent(number, exponent = 1) {
+    if (isNaN(number) || isNaN(exponent)) {
+        return 'Помилка! Ви ввели неправильні дані.';
+    }
+
+    return Math.pow(number, exponent);
+}
+
+const result = numberToExponent(number, exponent);
+
+alert(result);
