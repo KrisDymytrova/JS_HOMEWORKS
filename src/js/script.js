5b5968b71a751552da2f7514de5941c3cb3c3942
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-13 01:21:38 +0200

    HW 30 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+function price(literBorsch, oneKgPotatoPrice) {
+    const potatoWeight= Math.ceil((300 * literBorsch) / 1000);
+    const potatoPrice = potatoWeight * oneKgPotatoPrice;
+
+    return `Для ${literBorsch} літрів борщу Вам знадобиться ${potatoWeight} кілограмів картоплі, яка коштуватиме ${potatoPrice} грн.`;
+}
+
+console.log(price(48, 13));
