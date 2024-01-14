e632f2fe10b37a2c86ab2173967dd8d7e134c3b5
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-14 15:30:01 +0200

    HW 28 completed after lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,20 @@
 'use strict';
 
-function padString(str, length, symbol = '*', symbolSide = true) {
-    if (typeof str !== 'string') return 'Помилка: неправильний тип аргументу str або він відсутній';
-    if (typeof length !== 'number') return 'Помилка: неправильний тип аргументу length або він відсутній';
-    if (length < 0) return 'Помилка: значення аргументу length не може бути від\'ємним';
-    if (typeof symbol !== 'string') return 'Помилка: неправильний тип аргументу symbol або він відсутній';
-    if (symbol.length > 1) return 'Помилка: значення аргументу symbol не може бути більше одного символу';
-    if (typeof symbolSide !== 'boolean') return 'Помилка: неправильний тип аргументу symbolSide або він відсутній';
+const padString = function (str, length, symbol = '*', rightSide = true) {
+    if(typeof str !== 'string') throw new Error('Неправильний тип str або він відсутній');
+    if(typeof length !== 'number' || isNaN(length)) throw new Error('Неправильний тип length або він відсутній');
 
-    switch (true) {
-        case length < str.length:
-            return str.substring(0, length);
-        case length > str.length:
-            const symbols = symbol.repeat(length - str.length);
-            return symbolSide ? str + symbols : symbols + str;
-        default:
-            return symbolSide ? str + symbol : symbol + str;
-    }
+    if(str.length === length) return str;
+    if(str.length > length) return str.substring(0, length);
+
+    if(typeof symbol !== 'string' || symbol.length !==1 ) throw new Error('Symbol має бути string довжиною в 1 символ');
+    if(typeof rightSide !== 'boolean') throw new Error('Неправильний тип rightSide або він відсутній');
+
+    const symbols = symbol.repeat(length - str.length);
+    return rightSide ? str + symbols : symbols + str;
 }
 
-console.log(padString('привіт', 8));                               // 'привіт**'
-console.log(padString('привіт', 6, '*', false)); // '*привіт'
+console.log(padString('привіт', 8));                                // 'привіт**'
+console.log(padString('привіт', 6, '*', false));   // 'привіт'
 console.log(padString('привіт', 2));                               // 'пр'
 console.log(padString('привіт', 10, '$', false)); // '$$$$привіт'
