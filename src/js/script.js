17e85acf24faff880bda61a2bdb799c72f37b16d
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-19 17:21:29 +0200

    HW 31 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const uniqueNumbers = new Set();
+
+const getRandomNum = function () {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+const getUniqueNum = function () {
+    let randomNum;
+    do {
+        randomNum = getRandomNum();
+    } while (uniqueNumbers.has(randomNum));
+
+    uniqueNumbers.add(randomNum);
+    return randomNum;
+}
