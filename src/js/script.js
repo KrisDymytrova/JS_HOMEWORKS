b1a44f2cc02a2aa2f3a5995a4b80cf715d2800d6
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-23 00:12:31 +0200

    HW 34 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const createUser = function (name, age) {
+  const user = Object.create(userMethods)
+  user.name = name
+  user.age = age
+  return user
+}
+
+const userMethods = {
+  greet: function () {
+    console.log(`Hello, my name is ${this.name}`)
+  },
+  ageUser: function () {
+    console.log(`I'm ${this.age} years old`)
+  }
+}
+
+const user1 = createUser('Kristina', 33)
+const user2 = createUser('Dmytro', 25)
+
+user1.greet()
+user1.ageUser()
+user2.greet()
+user2.ageUser()
