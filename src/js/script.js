ae63aa00dae0b54c52f77fa3868eb357cdbf9a0b
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-30 00:42:39 +0200

    HW 38 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const updateQueryString = function (url, key, value) {
+  const urlObject = new URL(url)
+  urlObject.searchParams.set(key, value)
+
+  return urlObject.toString()
+}
+
+const newUrl = updateQueryString('https://example.com/search?q=query&sort=ascending&page=2', 'page', '100')
+
+console.log(newUrl)
