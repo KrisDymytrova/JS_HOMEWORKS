1e45195e5b1d88371528b65a92dd2e9dc5a0759d
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-06 02:15:50 +0200

    HW 41 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,41 @@
 'use strict'
+
+// Завдання 1
+
+const runCallbacks = function (callbacks, value) {
+  return callbacks.reduce((acc, currentCallback) => currentCallback(acc), value)
+}
+
+const addOne = function (x) {
+  return x + 1
+}
+
+const square = function (x) {
+  return x * x
+}
+
+const callbacks = [addOne, square, addOne]
+
+const result = runCallbacks(callbacks, 1)
+console.log(result)
+
+// Завдання 2
+
+const composeFunctions = (...functions) => x => functions.reduceRight((acc, func) => func(acc), x)
+
+const addOne2 = function (x) {
+  return x + 1
+}
+
+const square2 = function (x) {
+  return x * x
+}
+
+const multiplyByTwo = function (x) {
+  return x * 2
+}
+
+const composedFunction = composeFunctions(addOne2, square2, multiplyByTwo)
+
+const result2 = composedFunction(2)
+console.log(result2)
