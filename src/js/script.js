043f934e5e49f55be8afa8e7e9e07be00c18a4b6
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-14 02:24:28 +0200

    HW 43 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
 'use strict'
+
+// Task 1
+
+const getFactorial = function (n) {
+  return (n === 0 || n === 1) ? 1 : n * getFactorial(n - 1)
+}
+
+console.log(getFactorial(3))
+
+// Task 2
+
+const pow = function (num, degree) {
+  if (degree === 0) return 1
+  if (degree === 1) return num
+  if (degree > 1) return num * pow(num, degree - 1)
+  return 1 / pow(num, -degree)
+}
+
+console.log(pow(2, 3))
+
+// Task 3
+
+const sum = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    console.log('Помилка: a та b мають бути позитивними цілими числами')
+    return null
+  }
+
+  if (b === 0) return a
+
+  return sum(a ^ b, (a & b) << 1)
+}
+
+console.log(sum(2, 8))
+console.log(sum(1, -2))
