2fcc6f48d1a0a0ea204244f289ae5dbd057675dc
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-16 19:18:19 +0200

    HW 47 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+const myBind = function (func, context, ...args) {
+  return function (...newArgs) {
+    return func.apply(context, [...args, ...newArgs])
+  }
+}
+
+const greeting = function (greeting, punctuation) {
+  return `${greeting} ${this.firstName} ${this.lastName} ${punctuation}`
+}
+
+const person = {
+  firstName: 'Kristina',
+  lastName: 'Dymytrova'
+}
+
+const krisGreeting = myBind(greeting, person)
+
+console.log(krisGreeting('Hi', '!!!'))
