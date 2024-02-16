f7c44bd6a4e80fbb43821addc3e4c60283ab1c47
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-16 03:37:30 +0200

    HW 46 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const myApply = function (func, argsArray) {
+  if (typeof func !== 'function') {
+    throw new TypeError('Перший аргумент має бути викликаємою функцією')
+  }
+
+  if (!Array.isArray(argsArray)) {
+    throw new TypeError('Другий аргумент має бути масивом')
+  }
+
+  return func(...argsArray)
+}
+
+const sum = function (a, b) {
+  return a + b
+}
+
+console.log(myApply(sum, [7, 15]))
