132f73c87561489c540f9b32dab7b5abfcebdce5
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-19 01:50:13 +0200

    HW 48 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+// Task 1
+
+const sum = (a, b) => a + b
+
+console.log(sum(4, 5))
+
+// Task 2
+
+const strLength = str => str.length
+
+console.log(strLength('Good morning'))
+
+// Task 3
+
+const array = arr => arr.map(num => num + 1)
+
+console.log(array([1, 2, 3, 4, 5]))
