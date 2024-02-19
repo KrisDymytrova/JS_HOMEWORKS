070c325c73acc2befa4371c290ec0ea3072cf718
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-19 03:10:58 +0200

    HW 49 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict'
+
+// Task 1
+
+const animals = ['dog', 'cat', 'rabbit', 'hamster', 'bird', 'horse']
+
+const funcFilter = animals.filter(str => str.length > 5)
+
+console.log(funcFilter)
+
+// Task 2
+
+const numbers = [2, 7, 10, 12, 15, 17]
+
+const sumOfNum = numbers.reduce((acc, num) => num % 2 === 0 ? acc + num : acc, 0)
+
+console.log(sumOfNum)
+
+// Task 3
+
+const average = numbers.reduce((acc, num) => acc + num, 0) / numbers.length
+
+console.log(average)
