dba9506abdb022ba9e5f4b99bccd15fec2a52267
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-20 02:02:25 +0200

    HW 51 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const parseJSON = function (str) {
+  try {
+    return JSON.parse(str)
+  } catch (err) {
+    throw new Error('Invalid JSON string')
+  }
+}
+
+console.log(parseJSON('{"name": "John", "age": 30}'))
+console.log(parseJSON('{"name": "John", "age":'))
