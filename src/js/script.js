43706ae4df8d35663d974ff41069b553103c5c4f
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-02-29 01:47:31 +0200

    HW 52 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const ulList = document.getElementById('ulId')
+
+const liTextArr = []
+
+for (const li of ulList.getElementsByTagName('li')) {
+  console.log(li.textContent)
+
+  liTextArr.push(li.textContent)
+}
+
+console.log('Загальна кількість елементів li:', ulList.children.length)
+
+console.log('Масив з текстом кожного елемента li:', liTextArr)
