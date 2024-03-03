bc4e2ff64351194ba20c78b4241e53f4d20346cf
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-04 01:01:15 +0200

    HW 54 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const generateList = ((array) => {
+  const createList = (arr) => {
+    const ul = document.createElement('ul')
+
+    arr.forEach((element) => {
+      const li = document.createElement('li')
+
+      if (Array.isArray(element)) {
+        li.appendChild(createList(element))
+      } else {
+        li.textContent = element
+      }
+
+      ul.appendChild(li)
+    })
+
+    return ul
+  }
+
+  return createList(array)
+})([1, 2, [1.1, 1.2, 1.3], 3])
+
+document.body.appendChild(generateList)
