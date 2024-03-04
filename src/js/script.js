0382a82d28c82335ff1533ab628628213a996bdd
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-04 02:03:21 +0200

    HW 55 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 'use strict'
+
+const createTable = (function () {
+  return function createTable () {
+    const table = document.createElement('table')
+
+    for (let i = 0; i < 10; i++) {
+      const row = document.createElement('tr')
+
+      for (let j = 0; j < 10; j++) {
+        const value = i * 10 + j + 1
+
+        const cell = document.createElement('td')
+
+        cell.textContent = value.toString()
+
+        row.appendChild(cell)
+      }
+
+      table.appendChild(row)
+    }
+
+    document.body.appendChild(table)
+  }
+})()
+
+createTable()
