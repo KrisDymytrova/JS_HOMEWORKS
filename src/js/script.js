741ba87457de7513c645a1116990a92a4f0e2084
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-04 23:20:28 +0200

    HW 58 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const input = document.createElement('input')
+input.type = 'text'
+input.placeholder = 'Введіть число'
+
+const button = document.createElement('button')
+button.textContent = 'Перевірити'
+
+button.addEventListener('click', function () {
+  const number = parseInt(input.value)
+
+  if (!isNaN(number)) {
+    if (number % 2 === 0) {
+      alert('Парне число')
+    } else {
+      alert('Непарне число')
+    }
+  } else {
+    alert('Введіть число!')
+  }
+})
+
+document.body.append(input)
+document.body.append(button)
