e164859bab30f6bd5be02182c60718e2e6247fae
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-05 02:05:15 +0200

    HW 59 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const input = document.createElement('input')
+input.type = 'text'
+input.placeholder = 'Введіть текст'
+
+const button = document.createElement('button')
+button.textContent = 'Зберегти'
+
+const container = document.createElement('div')
+
+button.addEventListener('click', function () {
+  const text = input.value.trim()
+  if (text) {
+    const savedText = document.createElement('div')
+    savedText.innerHTML = text
+    container.prepend(savedText)
+    input.value = ''
+  }
+})
+
+document.body.append(input)
+document.body.append(button)
+document.body.append(container)
