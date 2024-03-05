f294c76f37051aef63d9fa6ba4af2100b3e42260
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-05 02:32:51 +0200

    HW 56.1 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const ul = document.createElement('ul')
+
+const addListItem = function () {
+  const listItemContent = prompt('Введите содержимое для нового пункта списка:', '')
+
+  if (listItemContent !== null && listItemContent !== '') {
+    const li = document.createElement('li')
+    li.textContent = listItemContent
+    ul.appendChild(li)
+
+    addListItem()
+  } else {
+    document.body.appendChild(ul)
+  }
+}
+
+addListItem()
