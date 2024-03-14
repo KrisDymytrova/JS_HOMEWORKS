e35cdcf5f846fd325f767db1fb35fb660e713cf6
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-15 01:12:58 +0200

    HW 60 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict'
+
+(function () {
+  const form = document.querySelector('[data-form]')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const message = document.getElementById('message')
+
+    if (name === '' || email === '' || password === '') {
+      message.textContent = 'Будь ласка, заповніть усі поля'
+    } else {
+      message.textContent = 'Реєстрація успішна'
+    }
+  })
+})()
