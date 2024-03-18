97582cf5e9ec2d12a8812ebc9a9b41fe55cb8b11
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-19 00:27:47 +0200

    HW 62 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,43 @@
-'use strict'
+'use strict';
+
+(function () {
+  const form = document.querySelector('#form')
+  const dataContainer = document.querySelector('.data-container')
+  const dataKey = 'formData'
+
+  const renderData = () => {
+    const data = JSON.parse(localStorage.getItem(dataKey))
+    if (!data) return
+
+    if (document.querySelector('.dataUL')) {
+      document.querySelector('.dataUL').remove()
+    }
+
+    const ul = document.createElement('ul')
+    ul.classList.add('dataUL')
+
+    for (const key in data) {
+      const li = document.createElement('li')
+      li.innerHTML = `<b>${key}:</b> <i>${data[key]}</i>`
+      ul.append(li)
+    }
+
+    dataContainer.append(ul)
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const elements = e.target.querySelectorAll('input, textarea, select')
+
+    const data = Array.from(elements).reduce((acc, item) => {
+      acc[item.name] = item.value
+      return acc
+    }, {})
+
+    localStorage.setItem(dataKey, JSON.stringify(data))
+    renderData()
+  })
+
+  document.addEventListener('DOMContentLoaded', renderData)
+}())
