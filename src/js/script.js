6a8caea504cdf3d23424af6302fc7812d98a968d
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-03-19 02:28:55 +0200

    HW 62 Add checks and display a noDataMessage

--- src/js/script.js
+++ src/js/script.js
@@ -7,37 +7,46 @@
 
   const renderData = () => {
     const data = JSON.parse(localStorage.getItem(dataKey))
-    if (!data) return
 
     if (document.querySelector('.dataUL')) {
       document.querySelector('.dataUL').remove()
     }
 
-    const ul = document.createElement('ul')
-    ul.classList.add('dataUL')
+    if (!data || Object.keys(data).length === 0 || Object.keys(data).filter(key => key !== 'option' && key !== 'terms').every(key => data[key] === '')) {
+      dataContainer.innerHTML = ''
+      const noDataMessage = document.createElement('p')
+      noDataMessage.textContent = 'No stored data found.'
+      noDataMessage.classList.add('noDataMessage')
+      dataContainer.appendChild(noDataMessage)
+    } else {
+      const ul = document.createElement('ul')
+      ul.classList.add('dataUL')
 
-    for (const key in data) {
-      const li = document.createElement('li')
-      li.innerHTML = `<b>${key}:</b> <i>${data[key]}</i>`
-      ul.append(li)
+      for (const key in data) {
+        const li = document.createElement('li')
+        li.innerHTML = `<b>${key}:</b> <i>${data[key]}</i>`
+        ul.append(li)
+      }
+
+      dataContainer.append(ul)
     }
-
-    dataContainer.append(ul)
   }
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
 
-    const elements = e.target.querySelectorAll('input, textarea, select')
+      const elements = e.target.querySelectorAll('input, textarea, select')
 
-    const data = Array.from(elements).reduce((acc, item) => {
-      acc[item.name] = item.value
-      return acc
-    }, {})
+      const data = Array.from(elements).reduce((acc, item) => {
+        acc[item.name] = item.value.trim()
+        return acc
+      }, {})
 
-    localStorage.setItem(dataKey, JSON.stringify(data))
-    renderData()
-  })
+      localStorage.setItem(dataKey, JSON.stringify(data))
+      renderData()
+    })
+  }
 
   document.addEventListener('DOMContentLoaded', renderData)
 }())
